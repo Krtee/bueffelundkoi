958b6000bdb74176115686d5c4830859e9c0c8d8
Minh Vu Nguyen <m.nguyen@devduck.de>
2024-02-07 00:31:48 +0100

    add speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
 import "react-datepicker/dist/react-datepicker.css";
@@ -15,6 +16,7 @@ import "./../styles/privacy.scss";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
+      <SpeedInsights />
       <Component {...pageProps} />
     </NextUIProvider>
   );
