1596b96ee9cfa91e6047d55df671f4d9faee587a
Minh Vu Nguyen <m.nguyen@devduck.de>
2024-08-13 22:27:07 +0200

    fix bugs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,6 @@ import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import "../styles/globals.scss";
 import "./../styles/booking.scss";
-import "./../styles/datetimepicker.scss";
 import "./../styles/preflight.css";
 import "./../styles/privacy.scss";
 
