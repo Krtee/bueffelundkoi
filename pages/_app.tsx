38295abb00a8c6de247c5ae4f8b03e97dab27535
Minh Vu Nguyen <m.nguyen@devduck.de>
2025-05-08 22:06:30 +0200

    revert changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 "use client";
 import { NextUIProvider } from "@nextui-org/system";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { appWithTranslation } from "next-i18next";
 import type { AppProps } from "next/app";
-import { Suspense } from "react";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import "../styles/globals.scss";
@@ -16,11 +13,7 @@ import "./../styles/privacy.scss";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
-      <SpeedInsights />
-      <Analytics />
-      <Suspense>
-        <Component {...pageProps} />
-      </Suspense>
+      <Component {...pageProps} />
     </NextUIProvider>
   );
 }
