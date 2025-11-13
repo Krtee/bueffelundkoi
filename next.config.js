bd670b8154e1a9fb3ea5fbb2825eaca8825e85a5
Minh Vu Nguyen <m.nguyen@devduck.de>
2025-11-13 14:45:44 +0100

    build changes

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { i18n } = require("./next-i18next.config");
 module.exports = {
   reactStrictMode: true,
   i18n,
+  transpilePackages: ["@heroui/react", "@heroui/sytem"],
   async headers() {
     return [
       {
