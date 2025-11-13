d41535fe8250ea072d37460ba3abd857631c2eba
Minh Vu Nguyen <m.nguyen@devduck.de>
2025-11-13 14:13:02 +0100

    fix build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  transpilePackages: ['@heroui/system','@heroui/react'],
   i18n:{
     defaultLocale: 'de',
     locales: ['en', 'de'],
